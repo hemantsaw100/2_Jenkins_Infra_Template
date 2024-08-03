67b9db8711feee286fbe199c7f57bb9f1e163553
Hemant Saw <84616397+hemantsaw100@users.noreply.github.com>
2024-08-03 22:44:14 +0530

    Updated remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -4,10 +4,5 @@ terraform {
     key    = "2_Jenkins_Infra_Template\terraform.tfstate"
     region = "ap-northeast-1" # Tokyo
 
-    tags = {
-      Name    = "Jenkins Infra Setup"
-      Section = "2"
-    }
-
   }
 }
