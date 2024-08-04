3bd3682096fa4d64f480ff45fbd7a3386b82a0f7
hemant <sawhemant41@gmail.com>
2024-08-04 16:57:06 +0530

    Comment out profile arg in provider file

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region                   = "ap-northeast-1"         # Tokyo
-  profile                  = "terraform-project-user" # IAM User
+  //profile                  = "terraform-project-user" # IAM User
   shared_credentials_files = ["C:/Users/hesaw/.aws/credentials"]
 }
