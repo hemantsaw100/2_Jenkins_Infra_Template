120b31ccb842a54b9aab9e3dde9a86cc8f477441
hemant <sawhemant41@gmail.com>
2024-08-15 19:20:06 +0530

    updated python flask requirement installation commd

--- infra/template/ec2_install_python.sh
+++ infra/template/ec2_install_python.sh
@@ -5,7 +5,7 @@ yes | sudo apt install python3 python3-pip
 git clone https://github.com/hemantsaw100/3_python_mysql_db.git # Python MySQL DB Git Repo
 sleep 20
 cd python-mysql-db-proj-1
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt --break-system-packages
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &
 sleep 30
